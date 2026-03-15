a31dbe08555526ad4a23426544498f7de1ab529d
brucemurphy <64602550+brucemurphy@users.noreply.github.com>
2026-03-15 10:17:19 -0700

    DISM Lab v2.1 03152026

--- Application.xaml.vb
+++ Application.xaml.vb
@@ -1,6 +1,12 @@
-﻿Class Application
+﻿Partial Public Class Application
+    Inherits System.Windows.Application
 
-    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
-    ' can be handled in this file.
+    Protected Overrides Sub OnStartup(e As StartupEventArgs)
+        Dim splash = New SplashScreen("SplashLab.png")
+        splash.Show(False)
+        System.Threading.Thread.Sleep(1500)
+        splash.Close(TimeSpan.Zero)
 
+        MyBase.OnStartup(e)
+    End Sub
 End Class
